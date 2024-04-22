4f1d761162aef8700e3a76d609492302bf7fc794
facundomala1 <f.mala@alumno.um.edu.ar>
2024-04-22 12:26:28 -0300

    finish code

--- factorial.py
+++ factorial.py
@@ -21,20 +21,20 @@ class TestFactorial(unittest.TestCase):
         self.assertEqual(resultado, 6)
     
     def test_con_4(self):
-        resultado = factorial(6)  # Cambiado de 4 a 6
-        self.assertEqual(resultado, 720)  # El factorial de 6 es 720
+        resultado = factorial(6) 
+        self.assertEqual(resultado, 720)  
     
     def test_con_5(self):
-        resultado = factorial(8)  # Cambiado de 5 a 8
-        self.assertEqual(resultado, 40320)  # El factorial de 8 es 40320
+        resultado = factorial(8) 
+        self.assertEqual(resultado, 40320) 
     
     def test_con_6(self):
-        resultado = factorial(10)  # Cambiado de 6 a 10
-        self.assertEqual(resultado, 3628800)  # El factorial de 10 es 3628800
+        resultado = factorial(10)  
+        self.assertEqual(resultado, 3628800)  
     
     def test_con_7(self):
-        resultado = factorial(12)  # Cambiado de 7 a 12
-        self.assertEqual(resultado, 479001600)  # El factorial de 12 es 479001600
+        resultado = factorial(12) 
+        self.assertEqual(resultado, 479001600)  
     
 
 
